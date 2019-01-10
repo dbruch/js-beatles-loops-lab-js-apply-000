0f201707488ff93296a1078c5063f8d1d3f7bc04
Dan Ruch <danbruch@gmail.com>
2019-01-10 00:40:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function theBeatlesPlay(musicianArr, instrumentArr) {
   var resultArr = [];
   for (var i = 0; i < musicianArr.length; i++) {
+<<<<<<< HEAD
     var str = `${musicianArr[i]} plays ${instrumentArr[i]}`;
+=======
+    var str = '`${musicianArr[i]} plays ${guitar}.`';
+>>>>>>> dc6a1ecbb626c41afbae6ad100512c1d2e2ceb23
     resultArr.push(str);
   }
   console.log(resultArr);
   return resultArr;
 }
 
+<<<<<<< HEAD
 function johnLennonFacts(factArray) {
   var i = 0;
   while (i < factArray.length) {
@@ -26,3 +31,5 @@ function iLoveTheBeatles(num) {
   } while (num < 15);
   return loveArray;
 }
+=======
+>>>>>>> dc6a1ecbb626c41afbae6ad100512c1d2e2ceb23
